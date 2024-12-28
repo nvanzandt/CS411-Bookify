4e460224c2f69dd39790efaf48506acc1c62187a
devnvanzandt@gmail.com <devnvanzandt@gmail.com>
2024-12-28 18:29:09 -0500

    Minor change

--- frontend/src/components/login-page/Login.js
+++ frontend/src/components/login-page/Login.js
@@ -17,7 +17,7 @@ function Login() {
         .then(response => response.json())
         .then(data => {
           if (data.success) {
-            window.location.href = data.redirect;
+            window.location.replace(data.redirect);
           } else {
             console.error('Auth error:', data.error);
             // Handle error appropriately
